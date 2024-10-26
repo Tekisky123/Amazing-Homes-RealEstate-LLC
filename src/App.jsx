ea83deaa4d506ad028c42c4e37f8834e7dafe744
shaikhmusa6448@gmail.com <shaikhmusa6448@gmail.com>
2024-10-26 12:50:24 +0530

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer";
 
 function App() {
   return <>
   <Navbar/>
+  <Footer/>
   </>
 }
 

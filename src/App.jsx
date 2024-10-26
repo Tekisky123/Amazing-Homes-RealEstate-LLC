933831a461da5f4712fbb12993ad9c86cfa9f322
suleman <sullukhan123786@gmail.com>
2024-10-26 14:22:11 +0530

    pull from musa

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./Components/Navbar"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import SliderComponent from "./Components/SliderComponent";
 
 function App() {
-  return <>
-  <Navbar/>
-  <Footer/>
-  </>
+  return (
+    <>
+      <Navbar />
+      <SliderComponent />
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;

ad84f677777a221435d33899a4982e14dcd11ffc
musashaikh6448.bootom-logo{ <shaikhmusa6448@gmail.com>
2024-10-28 12:41:02 +0530

    changes

--- src/Components/HeaderComponent.jsx
+++ src/Components/HeaderComponent.jsx
@@ -5,6 +5,8 @@ const HeaderComponent = () => {
     <header className="header">
       <nav className="navbar">
         <div className="navbar-logo">LOGO</div>
+        <div className="navbar-content">Home</div>
+        <div className="navbar-content">contact</div>
         <div className="navbar-search">
           <input type="text" placeholder="Search..." />
         </div>
